1b9721bcbaa8b8415a189cba23446891b95a7698
Andrew <legendarysuwe@gmail.com>
2022-04-10 15:27:12 +0300

    bugfix

--- components/Themechanger.jsx
+++ components/Themechanger.jsx
@@ -40,7 +40,7 @@ const ThemeChanger = () => {
           viewBox="0 0 24 24"
           stroke="currentColor"
           id="moon"
-          class="w-8 h-8 text-cool-gray-800 dark:text-cool-gray-200 group-hover:text-purple-600 group-focus:text-purple-600 dark:group-hover:text-purple-50 dark:group-focus:text-purple-50"
+          classname="w-8 h-8 text-cool-gray-800 dark:text-cool-gray-200 group-hover:text-purple-600 group-focus:text-purple-600 dark:group-hover:text-purple-50 dark:group-focus:text-purple-50"
         >
           <path
             stroke-linecap="round"
